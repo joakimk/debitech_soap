c850399930f525cf6ba19d209029eebf1922e910
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-01-02 17:25:58 +0100

    Adding some basics from our prototype implementation.

--- lib/debitech_soap.rb
+++ lib/debitech_soap.rb
@@ -1,9 +1,12 @@
+require 'soap/wsdlDriver'
+
 module DebitechSoap
-  class DebitechSoap
-    def self.test
-      
+  class API
+    RETURN_DATA = %w{aCSUrl acquirerAddress acquirerAuthCode acquirerAuthResponseCode acquirerCity acquirerConsumerLimit acquirerErrorDescription acquirerFirstName acquirerLastName acquirerMerchantLimit acquirerZipCode amount errorMsg infoCode infoDescription pAReqMsg resultCode resultText verifyID}
+
+    def initialize(opts = {})
+      @opts = opts
+      @client = SOAP::WSDLDriverFactory.new('https://secure.incab.se/axis2/services/DTServerModuleService_v1?wsdl').create_rpc_driver
     end
   end
-
-  # Your code goes here...
 end
